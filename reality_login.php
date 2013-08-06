4d25832aa40bb398ff748cd834880efc0be6bf78
Rodelle Ladia <rladia@gmail.com>
2013-08-05 21:02:15 -0700

    Login page redirects to about on successful login

--- reality_login.php
+++ reality_login.php
@@ -8,7 +8,7 @@ get_header(); ?>
 	<div id="primary" class="site-content wrapper">
 		<div class="login-container">
 				<?php $args = array(
-					'redirect'	=>	site_url()
+					'redirect'	=>	site_url( "about" )
 				); ?>
 				<?php wp_login_form( $args ); ?>
 				<a href="<?php echo wp_lostpassword_url(); ?>" title="Retrieve Your Password">Forgot Password?</a>
